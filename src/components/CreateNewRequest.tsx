601d34c9e40f4643bc22bc969a5642b1f8588abd
Dinesh Rajineni <dineshrajineni21@gmail.com>
2024-07-26 12:54:15 +0530

    ok

--- src/components/CreateNewRequest.tsx
+++ src/components/CreateNewRequest.tsx
@@ -1,162 +1,180 @@
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import { useNavigate } from 'react-router-dom';
-import projects from '../data/projects.json'
-import { useState } from 'react';
-
-
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { useNavigate } from "react-router-dom";
+import projects from "../data/projects.json";
+import { useState } from "react";
 
 function CreateNewRequest() {
-    const [selectedBU, setselectedBU] = useState('');
-    const [selectedLAB, setselectedLAB] = useState([]);
-    const [selectedTeam, setselectedTeam] = useState([]);
+  const [selectedBU, setselectedBU] = useState("");
+  const [selectedLAB, setselectedLAB] = useState([]);
+  const [selectedTeam, setselectedTeam] = useState([]);
 
-    const SelProjectCall = (e: any) => {
-        setselectedBU(e.target.value);
-        let labDetails: any = [];
-        projects.forEach(data => {
-            if (data.name === e.target.value) data.labDetails.forEach(lab => { labDetails.push(<option value={lab.name}>{lab.name}</option>) });
-        })
-        labDetails = [...labDetails];
-
-        let teamDetails: any = [];
-        let element:any = document.getElementById("Lab");
-        element.value = "Select";
-        element = document.getElementById("Team");
-        element.value = "Select";
-        setselectedLAB(labDetails);
-
-    };
-
-    const SelLabCall = (e: any) => {
-        let teamDetails: any = [];
-        let element:any = document.getElementById("Team");
-        element.value = "Select";
-        projects.forEach(proj => {
-            proj.labDetails.forEach(lab => {
-                if (lab.name === e.target.value) {
-                    lab.teamDetails.forEach(team => { teamDetails.push(<option value={team.id}>{team.name}</option>) })
-                }
-            });
+  const [a, setA] = useState(false);
+  const SelProjectCall = (e: any) => {
+    setselectedBU(e.target.value);
+    let labDetails: any = [];
+    projects.forEach((data) => {
+      if (data.name === e.target.value)
+        data.labDetails.forEach((lab) => {
+          labDetails.push(<option value={lab.name}>{lab.name}</option>);
         });
-        setselectedTeam(teamDetails);
-    };
+    });
+    labDetails = [...labDetails];
 
-    return (
+    let teamDetails: any = [];
+    let element: any = document.getElementById("Lab");
+    element.value = "Select";
+    element = document.getElementById("Team");
+    element.value = "Select";
+    setselectedLAB(labDetails);
+  };
 
-        <>
-            <h1 className='screenheader'>Create New Request</h1>
-            <form className='form'>
-                <div>
-                    <label>Provide Employee ID: </label>
-                    <input type='text' ></input>
-                </div>
-                <br />
-                <br />
-                <div>
-                    <label>Select Platform BU: </label>
-                    <select name="BU" id="BU" defaultValue={['Select']} onChange={(event) => SelProjectCall(event)}>
-                        <option value="Select">Select</option>
-                        {projects.map(proj => <option key={proj.name} value={proj.name}>{proj.name}</option>)}
-                    </select>
-                </div>
-                <br />
-                <br />
-                <div>
-                    <label>Select Lab: </label>
-                    <select name="Lab" id="Lab" onChange={(event) => SelLabCall(event)}>
-                    <option value="Select">Select</option>
-                    {selectedLAB}
-                    </select>
-                        {/* {selectedLAB.map(data => <option value={data}>{data}</option>)} */}
-                        {/* {projects.filter(proj => proj.name===selectedBU)[0].labDetails.map(lab => <option value={lab.name}>{lab.name}</option>)} */}
-                    {/* </select> */}
-                </div>
-                <br />
-                <br />
-                <div>
-                    <label>Select Feature Team: </label>
-                    <select name="Team" id="Team">
-                        <option value="Select">Select</option>
-                        {selectedTeam}
-                    </select>
-                </div>
-                <br />
-                <br />
-                <div>
-                    <label>Select Role: </label>
-                    <select name="Role" id="Role">
-                        <option value="QE">QE</option>
-                        <option value="Dev">Dev</option>
-                        <option value="Lead">Lead</option>
-                    </select>
-                </div>
-                <br />
-                <br />
-                <button className="lyds-button" >
-                    Submit Request
-                </button>
-            </form>
-        </>
+  const SelLabCall = (e: any) => {
+    let teamDetails: any = [];
+    let element: any = document.getElementById("Team");
+    element.value = "Select";
+    projects.forEach((proj) => {
+      proj.labDetails.forEach((lab) => {
+        if (lab.name === e.target.value) {
+          lab.teamDetails.forEach((team) => {
+            teamDetails.push(<option value={team.id}>{team.name}</option>);
+          });
+        }
+      });
+    });
+    setselectedTeam(teamDetails);
+  };
 
-
-    );
+  return (
+    <>
+      <h1 className="screenheader">Create New Request</h1>
+      <form className="form">
+        <div>
+          <label>Provide Employee ID: </label>
+          <input type="text"></input>
+        </div>
+        <br />
+        <br />
+        <div>
+          <label>Select Platform BU: </label>
+          <select
+            name="BU"
+            id="BU"
+            defaultValue={["Select"]}
+            onChange={(event) => SelProjectCall(event)}
+          >
+            <option value="Select">Select</option>
+            {projects.map((proj) => (
+              <option key={proj.name} value={proj.name}>
+                {proj.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <br />
+        <br />
+        <div>
+          <label>Select Lab: </label>
+          <select name="Lab" id="Lab" onChange={(event) => SelLabCall(event)}>
+            <option value="Select">Select</option>
+            {selectedLAB}
+          </select>
+          {/* {selectedLAB.map(data => <option value={data}>{data}</option>)} */}
+          {/* {projects.filter(proj => proj.name===selectedBU)[0].labDetails.map(lab => <option value={lab.name}>{lab.name}</option>)} */}
+          {/* </select> */}
+        </div>
+        <br />
+        <br />
+        <div>
+          <label>Select Feature Team: </label>
+          <select name="Team" id="Team">
+            <option value="Select">Select</option>
+            {selectedTeam}
+          </select>
+        </div>
+        <br />
+        <br />
+        <div>
+          <label>Select Role: </label>
+          <select name="Role" id="Role">
+            <option value="QE">QE</option>
+            <option value="Dev">Dev</option>
+            <option value="Lead">Lead</option>
+          </select>
+        </div>
+        <br />
+        <br />
+        <button
+          className="lyds-button"
+          onClick={() => alert("Submit successfully")}
+        >
+          Submit Request
+        </button>
+      </form>
+    </>
+  );
 }
 
 function BU() {
-    return (
-        <>
-            <div>
-                <label>Select Platform BU: </label>
-                <select name="BU" id="BU" onChange={e => (e.target.value)}>
-                    Select Platform:
-                    {projects.map(proj => <option>{proj.name}</option>)}
-                </select>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <label>Select Platform BU: </label>
+        <select name="BU" id="BU" onChange={(e) => e.target.value}>
+          Select Platform:
+          {projects.map((proj) => (
+            <option>{proj.name}</option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 }
 
 function LAB() {
-    return (
-        <>
-            <div>
-                <label>Select Lab: </label>
-                <select name="Lab" id="Lab">
-                    {projects[0].labDetails.map(lab => <option>{lab.name}</option>)}
-                </select>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <label>Select Lab: </label>
+        <select name="Lab" id="Lab">
+          {projects[0].labDetails.map((lab) => (
+            <option>{lab.name}</option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 }
 
 function FT() {
-    return (
-        <>
-            <div>
-                <label>Select Feature Team: </label>
-                <select name="FT" id="FT">
-                    {projects[0].labDetails[0].teamDetails.map(team => <option>{team.name}</option>)}
-                </select>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <label>Select Feature Team: </label>
+        <select name="FT" id="FT">
+          {projects[0].labDetails[0].teamDetails.map((team) => (
+            <option>{team.name}</option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 }
 
 function Role() {
-    return (
-        <>
-            <div>
-                <label>Select Role: </label>
-                <select name="Role" id="Role">
-                    <option value="QE">QE</option>
-                    <option value="Dev">Dev</option>
-                    <option value="Lead">Lead</option>
-                </select>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <label>Select Role: </label>
+        <select name="Role" id="Role">
+          <option value="QE">QE</option>
+          <option value="Dev">Dev</option>
+          <option value="Lead">Lead</option>
+        </select>
+      </div>
+    </>
+  );
 }
 
 export default CreateNewRequest;
--- src/components/EmployeeTickets.tsx
+++ src/components/EmployeeTickets.tsx
@@ -1,112 +1,111 @@
-import DataTable, { createTheme } from 'react-data-table-component';
-import dat from '../data/EmployeeTickets.json'
+import DataTable, { createTheme } from "react-data-table-component";
+import dat from "../data/EmployeeTickets.json";
 
-import axios from 'axios';
-import SnowJiraTableComponent from './SnowJiraTable';
+import axios from "axios";
+import SnowJiraTableComponent from "./SnowJiraTable";
 
+import dat2 from "../data/JIRAData.json";
+import { wrap } from "module";
+import GetTable from "./getTable";
+import { useEffect, useState } from "react";
 
-import dat2 from '../data/JIRAData.json'
-import { wrap } from 'module';
-import GetTable from './getTable';
-
-
-
-createTheme('solarized', {
-  text: {
-    primary: '#000000',
-    secondary: '#2aa198',
-  },
-  background: {
-    default: '#FFFFF',
-  },
-  context: {
-    background: 'rgb(53, 122, 72)',
-    text: '#000000',
-  },
-  headCells: {
-    style: {
-      paddingLeft: '8px', // override the cell padding for head cells
-      paddingRight: '8px',
+createTheme(
+  "solarized",
+  {
+    text: {
+      primary: "#000000",
+      secondary: "#2aa198",
+    },
+    background: {
+      default: "#FFFFF",
+    },
+    context: {
+      background: "rgb(53, 122, 72)",
+      text: "#000000",
+    },
+    headCells: {
+      style: {
+        paddingLeft: "8px", // override the cell padding for head cells
+        paddingRight: "8px",
+      },
+    },
+    divider: {
+      default: "#357a48",
+    },
+    action: {
+      button: "rgb(53, 122, 72)",
+      hover: "rgba(0,0,0,.08)",
+      disabled: "rgba(0,0,0,.12)",
     },
   },
-  divider: {
-    default: '#357a48',
-  },
-  action: {
-    button: 'rgb(53, 122, 72)',
-    hover: 'rgba(0,0,0,.08)',
-    disabled: 'rgba(0,0,0,.12)',
-  },
-}, 'light');
+  "light"
+);
 
 const columns = [
   {
-    name: 'Ticked ID',
-    selector: (row: { id: any; }) => {
+    name: "Ticked ID",
+    selector: (row: { id: any }) => {
       return row.id;
     },
     sortable: true,
-    
   },
   {
-    name: 'Ticket Status',
-    selector: (row: { status: any; }) => {
+    name: "Ticket Status",
+    selector: (row: { status: any }) => {
       return row.status;
     },
     sortable: true,
-    
   },
-  
 ];
 
-const data = dat;
-
-
-
-
 const ExpandedComponent = ({ data }: { data: any }) => {
- let a:string = JSON.stringify(data.ticketDetails);
-  return <GetTable data = {data.ticketDetails}/>
-   
+  let a: string = JSON.stringify(data.ticketDetails);
+  return <GetTable data={data.ticketDetails} />;
 };
 
-const SnowJiraTableComponent2 = ({ data, columns }: { data: any, columns: any }) => {
+const SnowJiraTableComponent2 = ({
+  data,
+  columns,
+}: {
+  data: any;
+  columns: any;
+}) => {
   return (
     <>
-    <h1 className='screenheader'>Employee DashBoard</h1>
-    <form className='form'>
-      <DataTable
-        columns={columns}
-        data={data}
-        theme="solarized"
-      />
-    </form>
+      <h1 className="screenheader">Employee DashBoard</h1>
+      <form className="form">
+        <DataTable columns={columns} data={data} theme="solarized" />
+      </form>
     </>
   );
 };
 
-const func =  async ()=>{
-  const data = await axios.get('http://localhost:8890/api/requests/getUserRequests/15');
-  return data;
-}
-let a= func();
-
-console.log(a);
-
 export default function EmployeeTicketsTable() {
-  
+  const [data, setData] = useState([{ id: "1", status: "done" }]);
 
-
+  const func = async () => {
+    const data = await axios.get(
+      "http://localhost:8890/api/requests/getUserRequests/15"
+    );
+    return data.data;
+  };
+  useEffect(() => {
+    let a;
+    (async () => {
+      a = await func();
+      setData(a);
+    })();
+  }, [columns]);
 
   return (
-    <form className='form'>
+    <form className="form">
       <DataTable
-        columns={columns}
-        data={data}
+        columns={columns || []}
+        data={data || []}
         theme="solarized"
         expandableRows={true}
         expandableRowsComponent={ExpandedComponent}
       />
     </form>
   );
-};
+}
